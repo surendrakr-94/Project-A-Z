89d6ff97c44fc3eb9bbe5723440983ec70fc810e
omsemwal <omsemwal111@gmail.com>
2022-09-08 10:33:59 +0530

    login

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,5 +1,6 @@
+const authorModel = require("../models/authorModel")
 const AuthorModel = require("../models/authorModel")
-
+const jwt = require('jsonwebtoken')
 const createAuthor = async function (req, res) {
     let author = req.body
     let authorCreated = await AuthorModel.create(author)
@@ -11,5 +12,23 @@ const getAuthorsData = async function (req, res) {
     res.send({ data: authors })
 }
 
+const login = async function (req, res) {
+    let Email = req.body.email
+    let Password = req.body.password
+    let data = await AuthorModel.findOne({ email: Email, password: Password })
+    if (!data) { return res.status(400).send({ msg: "email and password is incorrect" }) }
+
+    let token = await jwt.sign({ id: data._id.toString() }, "functionupiswaywaycoolHariomSemwal")
+    console.log(token)
+    res.header({ "x-powered-by": token })
+    res.status(200).send({ status: true, msg: token })
+
+    // const verifiy = await jwt.verify(token, "functionupiswaywaycoolHariomSemwal")
+    // if (!token) { return res.status(404).send({ status: false.valueOf, msg: "token is in valid" }) }
+    // console.log(verifiy)
+}
+
+
 module.exports.createAuthor = createAuthor
 module.exports.getAuthorsData = getAuthorsData
+module.exports.login = login
