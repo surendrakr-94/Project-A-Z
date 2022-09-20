9a919cbeb2dbdce1ca41ddf367a99d94fc8813a1
RINTU5500U <crowdrintu1@gmail.com>
2022-09-20 12:30:08 +0530

    done

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const route = require('./routes/route');
+const  mongoose  = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://Sushant_Bhaiswar_30:WBYUu1bCYmxmZUmg@cluster0.jui41on.mongodb.net/group17Database?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route)
+
+app.listen(3000, function () {
+    console.log('Express app running on port ' + (3000))
+});
